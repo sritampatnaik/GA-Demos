a1a50a6807f9ed0940ace26279711381ce4de4cf
Sritam Patnaik <sritampatnaik@gmail.com>
2022-10-20 17:00:00 +0800

    W10D02

---
+++ 5-fetch/script.js
@@ -0,0 +1,15 @@
+// const apiCall = () => {
+//     fetch("http://www.omdbapi.com/?apikey=98e3fb1f&t=Eraserhead")
+//     .then(res => res.json())
+//     .then(res => console.log(res))
+// }
+
+// apiCall()
+
+// const apiCall = async () => {
+//     let res = await fetch("http://www.omdbapi.com/?apikey=98e3fb1f&t=Eraserhead")
+//     let json = await res.json()
+//     console.log(json)
+// }
+
+// apiCall()
